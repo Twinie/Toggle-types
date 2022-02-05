366e3fe962d49f036c60bc3d7ed07ac75a51c15e
Twinkle <twinklegohilchauhan@outlook.com>
2022-02-05 15:22:33 +0530

    added the tags as required

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,18 +1,24 @@
 
 $(".hamburger-menu").click(function(){
+    $(".hamburger-menu").toggleClass("menu-dms")
     $(".bar-board").toggleClass("bar-hide")
     $(".about-links").css("display", "none");
 })
 $(".nav-link-about").click(function(){
     $(".about-links").toggleClass("slide-up")
     $(".about-links").slideDown("slow")
-        $(".fa-chevron-down").css("transform", "rotate(90deg)")
+        $(".fa-angle-left").css("transform", "rotate(-90deg)")
     
 
         $(".nav-link-about").click(function(){
    if($(".about-links").hasClass("slide-up")){
        $(".about-links").slideUp("slow")
-        $(".fa-chevron-down").css("transform", "rotate(0deg)")
+        $(".fa-angle-left").css("transform", "rotate(0deg)")
    }
 });
+})
+
+$(".profile").click(function(){
+    $(".card").toggleClass("card-dms")
+    
 })
